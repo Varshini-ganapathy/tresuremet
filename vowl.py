2c5d3d7469979a47cc44ecb17cbfb340a69cf9d5
Varshini-ganapathy <41140681+Varshini-ganapathy@users.noreply.github.com>
2018-07-12 14:17:17 +0530

    Rename vowel.py to vowl.py

---
+++ vowl.py
@@ -0,0 +1,8 @@
+list1=['a','e','i','o','u','A','E','I','O','U']
+let=input()
+if let in list1:
+    print("Vowel")
+elif(ord(let)>=65 and ord(let)<=90 or ord(let)>=97 and ord(let)<=122):
+    print("Consonent")
+else:
+    print("invalid")

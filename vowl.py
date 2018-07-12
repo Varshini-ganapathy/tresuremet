0e4053810fc7235bc0cd325bd73159e63997645f
Varshini-ganapathy <41140681+Varshini-ganapathy@users.noreply.github.com>
2018-07-12 14:18:32 +0530

    Update vowl.py

--- vowl.py
+++ vowl.py
@@ -3,6 +3,6 @@ let=input()
 if let in list1:
     print("Vowel")
 elif(ord(let)>=65 and ord(let)<=90 or ord(let)>=97 and ord(let)<=122):
-    print("Consonent")
+    print("Consonant")
 else:
     print("invalid")
